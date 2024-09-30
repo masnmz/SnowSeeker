9abad1ee031d1db598ac57ff71fc301ac81d40fc
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-09-30 17:39:12 +0100

    Using groups as transparent layout containers

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -7,27 +7,33 @@
 
 import SwiftUI
 
-struct User: Identifiable {
-    var id = "Taylor Swift"
+struct UserView: View {
+    var body: some View {
+        Group {
+            Text("Name: Paul")
+            Text("Country: England")
+            Text("Pets: Luna and Arya")
+        }
+        .font(.title)
+        
+    }
     
 }
 
 struct ContentView: View {
-    @State private var selectedUser: User? = nil
-    @State private var isShowingUser = false
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
     
     var body: some View {
-        Button("Tap me ") {
-            selectedUser = User()
-            isShowingUser = true
+        ViewThatFits {
+            Rectangle()
+                .fill(.red)
+                .frame(width: 500, height: 200)
+            Circle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
         }
-        .sheet(item: $selectedUser) { user in
-            Text(user.id)
-                .presentationDetents([.height(60), .large])
         }
     }
-}
-
 #Preview {
     ContentView()
 }
