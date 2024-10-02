7ac1f7a8b220102e3bcdc3d5f9820338c4b2fe2a
Mehmet Alp Sonmez <mehmetsnmz95@gmail.com>
2024-10-02 14:47:58 +0100

    Making a SwiftUI view searchable

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -7,30 +7,27 @@
 
 import SwiftUI
 
-struct UserView: View {
-    var body: some View {
-        Group {
-            Text("Name: Paul")
-            Text("Country: England")
-            Text("Pets: Luna and Arya")
-        }
-        .font(.title)
-        
-    }
-    
-}
 
 struct ContentView: View {
-    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @State private var searchText = ""
+    let allNames = ["Subh", "Vina", "Melvin", "Stefanie"]
     
+    var filteredNames: [String] {
+        if searchText.isEmpty {
+            allNames
+        } else {
+            allNames.filter { name in
+                name.localizedStandardContains(searchText)
+            }
+        }
+    }
     var body: some View {
-        ViewThatFits {
-            Rectangle()
-                .fill(.red)
-                .frame(width: 500, height: 200)
-            Circle()
-                .fill(.blue)
-                .frame(width: 200, height: 200)
+        NavigationStack {
+            List(filteredNames, id: \.self) { name in
+                Text(name)
+            }
+            .searchable(text: $searchText, prompt: "Look for something")
+            .navigationTitle("Searching")
         }
         }
     }
